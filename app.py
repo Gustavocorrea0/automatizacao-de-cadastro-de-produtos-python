4b5dfa3e541b6ef072002bc33ef4fda054a13a16
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2024-04-03 15:56:11 -0300

    automatizacao de cadastro de usuarie e de cadastro de produtos

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 import pyautogui
 from time import sleep
 
+# Criar novo usuario
+pyautogui.click(704, 442, duration=0.5)
+
+pyautogui.click(701, 384, duration=0.5)
+pyautogui.write('admin')
+
+pyautogui.click(680, 411, duration=0.5)
+pyautogui.write('123456')
+
+pyautogui.click(665, 440, duration=2)
+
 # Clicar e Digitar usuario
 #         - Coordenadas | Duracao
-pyautogui.click(771,387, duration=2)
-pyautogui.write('gustavo')
+pyautogui.click(771, 387, duration=2)
+pyautogui.write('admin')
 
 # Clicar e Digitar senha
-pyautogui.click(675,411, duration=2)
+pyautogui.click(675, 411, duration=2)
 pyautogui.write('123456')
 
 # Clicar em Entrar
-pyautogui.click(593,441, duration=2)
+pyautogui.click(593, 441, duration=2)
+
+
+# Abrir arquivos
+#       Nome do Arquivo | Tipo: Leitura | Nome da atividade
+with open('produtos.txt', 'r') as arquivo:
+    # Pegar dados
+    for linha in arquivo:
+        produto = linha.split(',')[0]
+        quantidade = linha.split(',')[1]
+        preco = linha.split(',')[2]
+        
+        # Clicar e Digitar o produto
+        pyautogui.click(392, 372, duration=2)
+        pyautogui.write(produto)
+
+        pyautogui.click(391, 398, duration=2)
+        pyautogui.write(quantidade)
+
+        pyautogui.click(396, 426, duration=2)
+        pyautogui.write(preco)
+
+        pyautogui.click(323, 578, duration=1)
+        sleep(1)
+
